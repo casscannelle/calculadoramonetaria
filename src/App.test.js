4780b7d4ad968d86076f074d4501806a023cb21d
casscannelle <cassia.maffei@gmail.com>
2024-05-28 15:51:27 -0300

    delete page teste de teste

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Page from './components/Page/Page';
-
-test('renders titulo', () => {
-  render(<Page />);
-  const textElement = screen.getByText(/learn/i);
-  expect(textElement).toBeInTheDocument();
-});
